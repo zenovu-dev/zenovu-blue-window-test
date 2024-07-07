c7324c3e196cb3eeb0c4b3e587868a8ed7ce128a
zenovu-dev <zenovu.dev@gmail.com>
2024-07-07 17:57:04 +0300

    Update Button.jsx

--- src/components/partials/Button.jsx
+++ src/components/partials/Button.jsx
@@ -1,6 +1,8 @@
 const ButtonComponent = ({ label }) => {
 	return (
-		<button className="w-full bg-[#009129] p-5 rounded-[10px] font-inter text-white text-xl font-bold">Bet Now</button>
+		<button className="w-full bg-[#009129] p-5 rounded-[10px] font-inter text-white text-xl font-bold">
+			{ label }
+		</button>
 	)
 };
 
